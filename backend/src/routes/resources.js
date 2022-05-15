c1fb5435ab212f88b19307ac37b5e83139c3df82
A K M <mauryaankit907@gmail.com>
2022-05-15 16:33:33 +0530

    implemente getResources

--- backend/src/routes/resources.js
+++ backend/src/routes/resources.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { addResource } = require('../controllers/resource');
+const { addResource, getResources } = require('../controllers/resource');
 
 const router = express.Router();
 
 // router.get('/post' , getPost) ;
 
 router.post('/resources' , addResource) ;
-
+router.get("/resources",getResources)
 module.exports = router;
