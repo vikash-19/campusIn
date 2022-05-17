c4d3f529da589689b3809992f807789885fc9cc3
vikash prajapati <vikashprajapati1923@gmail.com>
2022-05-17 20:21:42 +0530

    Added procfile and modified port

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,14 @@ app.get('/' , (req , res) => {
     res.send('hello');
 })
 
-app.listen(3000 , function() {
-    console.log('Server is running on port 3000');
-})
+// app.listen(3000 , function() {
+//     console.log('Server is running on port 3000');
+// })
 
+
+port  =  process.env.PORT ;
+if(port == null || port == ""){
+    port =  3000 ;
+}
+
+app.listen(port) ;
