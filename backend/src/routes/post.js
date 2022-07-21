5c67d260acce7db11233b30f191150482d72af57
ankitmaurya19 <63341749+ankitmaurya19@users.noreply.github.com>
2022-07-21 21:38:21 +0530

    Added image upload

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const multer = require('multer');
+
+  const upload = multer({dest : "./uploads"});
+
 const { createPost, getPost } = require('../controllers/post');
 const router = express.Router();
 
 router.get('/post' , getPost) ;
 
-router.post('/post' , createPost) ;
+router.post('/post' , upload.single('avatar') , createPost) ;
 
 module.exports = router;
