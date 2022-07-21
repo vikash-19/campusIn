47f91e5ccbcff379c44dcc8bf901217f48aa2c35
ankitmaurya19 <63341749+ankitmaurya19@users.noreply.github.com>
2022-07-21 21:40:47 +0530

    Added image upload

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -2,41 +2,86 @@
 const Post = require('../models/post'); 
 const User = require('../models/user') ;
 
-exports.createPost =  function(req , res){
+const cloudinary = require('cloudinary');
 
-    let postId 
+cloudinary.config({ 
+    cloud_name: 'campusin', 
+    api_key: '612178527339326', 
+    api_secret: 'euqR2dspSvjHzxkvOhgAVEmP0CA' 
+  });
+
+const AddData = (req , contentFile) => {
+
+    let postId;
     const newPost  = new Post({
         userId : req.body.userId ,
-        contents : req.body.contents 
+        contents : req.body.contents ,
+        files : contentFile
     });
 
     newPost.save(function(err , data){
         if(err){
-            res.send(err) ;
+            console.log(err);
+            return -1;
         }
         else{
-            postId =  data.id ;
-            res.send(data) ;
+            postId = data._id ;
+            
+            User.findOne({userId : req.body.userId},function(error,foundList){
+                if(foundList !== null) {
+                    foundList.postIds.push(postId);
+                    foundList.save();
+                }
+            });
+
+            return 1;
         }
     })
+    
+}
 
+exports.createPost = async function(req , res){
 
-    User.findOne({userId : req.body.userId},function(err,foundList){
-        foundList.postIds.push(postId);
-        foundList.save();
-      });
-
-
+    if(req.file && (req.file.mimetype).includes('image')) {
+        cloudinary.v2.uploader.upload(req.file.path,
+        { public_id: req.file.filename }, 
+        function(error, result) {
+            let savedData = AddData(req , result.url);
+            if(savedData == -1) {
+                res.status(500).send('Error Occured');
+            } else {
+                res.status(200).send('Successful')
+            }
+        });
+    }
+    else if(req.file && req.file.mimetype.includes('video')) {
+        // use cloudinary to upload video
+    }
+    
 }
 
 
 exports.getPost = (req ,  res) => {
+    let start = req.query.next;
+    let end = start + 10;
     Post.find({} , (err , data)=>{
         if(err){
-            res.send(err) ;
-        }
-        else{
-            res.send(data) ;
+            res.status(500).send(err);
+        } else {
+            let n = data.length;
+            if(start >= n) {
+                res.status(200).send([]);
+            } else {
+                if(n < end) {
+                    end = n;
+                }
+                let requiredData = [];
+                for(let i = start ; i < end ; i++)
+                {
+                    requiredData.push(data[i]);
+                }
+                res.status(200).send(requiredData);
+            }
         }
     })
 }
